633a10f32b8d5a9e46311787eb9d6ae3f877a448
Ajay R. Mall <mallajay19@gmail.com>
2019-11-29 14:31:28 +0530

    Update crud.ts

--- crud.ts
+++ crud.ts
@@ -1,15 +1,15 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as express from "express";
-
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { db } from './init';
 
 const app = express();
+const cors = require('cors');
 
-export const webApi = functions.https.onRequest(app);
+app.use(cors({ origin: true }));
 
-// Get all products
+export const crudApi = functions.https.onRequest(app);
+
+//GET all product With Id
 app.get('/products', async (req, res) => {
   try {
     const docs = await db.collection('products').get();
@@ -22,54 +22,54 @@ app.get('/products', async (req, res) => {
       })
     );
   } catch (error) {
-    res.status(400).send(`Cannot get products: ${error}`);
+    res.status(400).send(`Cannot get contacts: ${error}`);
   }
 });
 
 // Get a Product byt ID
-app.get("/products/:id", async (req, res) => {
+app.get('/products/:id', async (req, res) => {
   try {
     const productId = req.params.id;
     await db
-      .collection("products")
+      .collection('products')
       .doc(`${productId}`)
       .get()
-      .then(doc => res.status(200).json({ doc }));
+      .then((doc) => res.status(200).json({ doc }));
   } catch (error) {
-    res.status(400).send(`Cannot get products: ${error}`);
+    res.status(400).send(`Cannot get contacts: ${error}`);
   }
 });
 
 // Add new product
-app.post("/products", async (req, res) => {
+app.post('/products', async (req, res) => {
   try {
-    const products = await db.collection("products").add({
-      name: req.body["name"],
-      category: req.body["category"],
-      brand: req.body["brand"]
+    const products = await db.collection('products').add({
+      name: req.body['name'],
+      categories: req.body['categories'],
+      brand: req.body['brand']
     });
-    res.status(200).send(`Created a new products: ${products.id}`);
+    res.status(200).send(`Created a new product: ${products.id}`);
   } catch (error) {
-    console.log(error);
+    console.log('Error From post req', error);
     res.status(404).send(error);
   }
 });
 
 // Update Product by Id
-app.put("/products/:id", async (req, res) => {
+app.put('/products/:id', async (req, res) => {
   try {
     const productId = req.params.id;
     const data = {
-      name: req.body["name"],
-      category: req.body["category"],
-      brand: req.body["brand"]
+      name: req.body['name'],
+      category: req.body['category'],
+      brand: req.body['brand'],
     };
 
     const updatedDoc = await db
-      .collection("products")
+      .collection('products')
       .doc(productId)
       .set(data, { merge: true });
-    res.status(200).send(`Update a new product: ${updatedDoc}`);
+    res.status(200).send(`Update a new contact: ${updatedDoc}`);
   } catch (error) {
     console.log(error);
     res.status(404).send(error);
@@ -77,11 +77,11 @@ app.put("/products/:id", async (req, res) => {
 });
 
 // Delete a contact
-app.delete("/products/:id", async (req, res) => {
+app.delete('/products/:id', async (req, res) => {
   try {
     const productId = req.params.id;
     const deletedProduct = await db
-      .collection("products")
+      .collection('products')
       .doc(productId)
       .delete();
     res.status(200).send(`Product is deleted: ${deletedProduct}`);
